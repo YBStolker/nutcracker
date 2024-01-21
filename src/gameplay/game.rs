2bff05d977cb694ec3650f79cb0f3f84c2c27523
Yannick Stolker <ystolker@gmail.com>
2024-01-21 02:09:38 +0100

    Start of basic game mechanics

--- src/gameplay/game.rs
+++ src/gameplay/game.rs
@@ -4,6 +4,7 @@ use super::player::Player;
 
 #[derive(Debug, Clone)]
 pub enum GameEvent {
+    StartStack(u32),
     Bet(u32),
     Flop(Cards),
     Turn(Cards),
@@ -19,19 +20,39 @@ pub struct Game {
 }
 
 impl Game {
-    pub fn new_round(&mut self) {
-        self.deck = Cards::from(FULL_DECK);
-        for p in self.players.iter() {
+    pub fn new(&mut self) -> Option<()> {
+        if self.players.len() < 2 {
+            return None;
         }
+
+        let first_player = self.players.remove(0);
+        self.players.push(first_player);
+
+        for i in 0..self.players.len() {
+            let player = self
+                .players
+                .get((i + self.dealer) % self.players.len())
+                .unwrap();
+
+            self.game_history
+                .push(GameEvent::StartStack(player.stack()))
+        }
+
+        self.deck = Cards::from(FULL_DECK);
+
+        Some(())
     }
 
-    pub fn add_player(&mut self, player: Player) -> () {
+    pub fn add_player(&mut self, player: Player) -> Option<()> {
+        if player.stack() < 100 {
+            return None;
+        }
+
         self.players.push(player);
+        Some(())
     }
 
-    pub fn add_bet(&mut self, bet: u32) -> () {
+    pub fn add_bet(&mut self, bet: u32) {
         self.game_history.push(GameEvent::Bet(bet));
-
     }
 }
-
--- src/gameplay/player.rs
+++ src/gameplay/player.rs
@@ -8,4 +8,12 @@ impl Player {
     pub fn new(name: String, stack: u32) -> Self {
         Self { name, stack }
     }
+
+    pub fn name(&self) -> &str {
+        self.name.as_ref()
+    }
+
+    pub fn stack(&self) -> u32 {
+        self.stack
+    }
 }
